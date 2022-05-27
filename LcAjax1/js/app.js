f578dc89157be55c07e53a96ee0b140fe01e8a25
Luiz Cauã <100131796+l0uzz@users.noreply.github.com>
2022-05-27 14:31:55 -0300

    Add files via upload

---
+++ LcAjax1/js/app.js
@@ -0,0 +1,14 @@
+/*$("#jt"lick(function(){
+    alert("C).clicou no botão");
+}).hover(function(){
+   $(this).css({"background-color": "blue"}); 
+}, function(){
+    $(this).css({"background-color": "white"});
+});*/
+$("#jt").click(
+    function(){
+        $.ajax({url: "index.php", sucess: function(result
+            $("#")
+            )});
+    }
+);
